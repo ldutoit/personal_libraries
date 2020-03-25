1dd2e9da039990a1825a582d209b98947299231b
Ludovic Dutoit <dutoit.ludovic@gmail.com>
2020-03-25 19:44:18 +1300

    Update summary_coverage_vcf.R

--- standalonescripts/summary_coverage_vcf.R
+++ standalonescripts/summary_coverage_vcf.R
@@ -6,16 +6,16 @@ vcf_file  =  #Path to your vcf i.e. "myvcf.vcf"
 ### Dependencies
 library("VariantAnnotation") # install from https://bioconductor.org/packages/release/bioc/html/VariantAnnotation.html​
 
+
+####Define functions
+#​Define a function that counts the number of missing data
+numbermissing<-function(x){ 
+    return(length(grep("\\./\\.",x)))
+}
 #Define the function
 summary_coverage_vcf<-function(vcf_file){
   print ("Reading data...")
   data<-readVcf(vcf_file) # Read the data
-
-  #​Define a function that counts the number of missing data
-  numbermissing<-function(x){ 
-    return(length(grep("\\./\\.",x)))
-  }
-
   # Count the  missing and then infer the non-missing
   countsofmissing<-apply(geno(data)$GT,2,numbermissing)
   nonmissing<-dim( geno(data)$GT)[1]-countsofmissing
